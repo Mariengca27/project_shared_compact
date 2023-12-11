2ceedafc71b7481bc57a275041fe45ed420cdd52
Maria Eduarda <mariaeduardacorrea.mg@gmail.com>
2023-12-11 09:38:14 -0300

    model-implementation

---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace GerenciadorDeProdutosCRUD.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Product Name")]
+        public string ProductName { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+        [Required]
+
+        public int Qty { get; set; }
+    }
+}
